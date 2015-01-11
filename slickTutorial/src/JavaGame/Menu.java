4bb1cfe019e0ed5ed50fd19660efef8f29a2e0e7
mps529 <slavin.matt529@gmail.com>
2015-01-11 15:46:39 -0500

    state switching

--- slickTutorial/src/JavaGame/Menu.java
+++ slickTutorial/src/JavaGame/Menu.java
@@ -9,34 +9,42 @@ import org.newdawn.slick.state.*;
  */
 public class Menu extends BasicGameState {
 
-
+    Image playNow;
+    Image exitGame;
 
     public Menu(int state){
 
     }
 
     public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        playNow = new Image("slickTutorial/res/playNow.png");
+        exitGame = new Image("slickTutorial/res/exitGame.png");
 
 
     }
 
     public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
-       g.fillOval(75, 100, 100, 100);
-       g.drawString("Play Now!", 80, 70);
+        g.drawString("Welcome to Juice Boys II", 100, 50);
+        playNow.draw(100, 100);
+        exitGame.draw(100, 200);
 
     }
 
     public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
-        Input input = gc.getInput();
-        int xPos = Mouse.getX();
-        int yPos = Mouse.getY();
-        if((xPos >75 && xPos <175)&&(yPos >160 && yPos <260)){
-            if(input.isMouseButtonDown(0)) {
+        int posX = Mouse.getX();
+        int posY = Mouse.getY();
+
+        //playNow button
+        if((posX >100 && posX<311) && (posY>209 && posY<260)) {
+            if(Mouse.isButtonDown(0)) {
                 sbg.enterState(1);
             }
-
         }
-
+        if((posX >100 && posX<311) && (posY>109 && posY<160)) {
+            if(Mouse.isButtonDown(0)) {
+                System.exit(0);
+            }
+        }
 
     }
 
